ab5d946301b33489da87b6b686e570ce1d4836bb
jzakotnik <jure@zakotnik.de>
2023-04-05 23:05:05 +0200

    initial import of book titles

--- README.md
+++ README.md
@@ -7,12 +7,17 @@ Die einfache Software für die Schulbibliothek
 - Start podman with docker compose
 - Open myphpadmin, create openbiblio database
 - Execute script to copy mySQL files
-- Use API to import data `curl -X POST -H "Content-Type: application/json" -d @member.json http://localhost:3000/api/openbiblioimport/migrate`
+- Use API to import members `curl -X POST -H "Content-Type: application/json" -d @member.json http://localhost:3000/api/openbiblioimport/migrateUsers`
+
+- Copy 3 book jsons together to one book_all.json, this includes the exports for tables `biblio.json`, `biblio_status_hist.json` and `usmarc_subfield_dm.json`
+- Use API to import books `curl -X POST -H "Content-Type: application/json" -d @book_all.json  http://localhost:3000/api/openbiblioimport/migrateBooks`
 
 
 
 Import members via `SELECT * FROM member`. Export as json. 
 
+
+## this does not work
 Import books via 
 ```sql
 SELECT `biblio_copy`.*, `biblio`.*, `biblio_status_hist`.* FROM `biblio_copy` , `biblio`, `biblio_status_hist` WHERE biblio_copy.bibid = biblio.bibid AND biblio.bibid = biblio_status_hist.bibid;
@@ -30,6 +35,12 @@ SELECT `usmarc_subfield_dm`.*, `usmarc_tag_dm`.*
 FROM `usmarc_subfield_dm`
 	, `usmarc_tag_dm` WHERE usmarc_tag_dm.tag = usmarc_subfield_dm.tag
 ```
+
+Sample book:
+```json
+{"bibid":"2185","copyid":"2185","copy_desc":"","barcode_nmbr":"2185","status_cd":"out","status_begin_dt":"2006-06-09 10:17:18","due_back_dt":"2006-06-30","mbrid":"1035","renewal_count":"0","create_dt":"2005-05-24 20:08:39","last_change_dt":"2023-01-27 10:56:54","last_change_userid":"4","material_cd":"2","collection_cd":"6","call_nmbr1":"Bücherei","call_nmbr2":"","call_nmbr3":"","title":"Die Wilden Fußballkerle. Bd. 09: Joschka, die siebte Kavallerie","title_remainder":"","responsibility_stmt":"","author":"Masannek, Joachim","topic1":"Fußball","topic2":"Teamgeist","topic3":"","topic4":"","topic5":"","opac_flg":"Y"},
+```
+
 
 
 These fields are migrated:
--- pages/api/openbiblioimport/migrateBooks.ts
+++ pages/api/openbiblioimport/migrateBooks.ts
@@ -5,6 +5,14 @@ import { addBook } from "@/entities/book";
 
 const prisma = new PrismaClient();
 
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "4mb", // Set desired value here
+    },
+  },
+};
+
 type Data = {
   data: string;
 };
@@ -13,8 +21,9 @@ type book = {
   book: BookType;
 };
 
+//this is some sample data for the migration
 const sampleBook = {
-  id: 1,
+  id: 2,
   rentalStatus: "out",
   rentedDate: new Date(),
   dueDate: new Date(),
@@ -40,6 +49,37 @@ const sampleBook = {
   maxAge: "89",
   additionalMaterial: "CD",
   price: 3,
+  userId: 1356,
+};
+
+const sampleBookFromOpenBiblio = {
+  bibid: "2185",
+  copyid: "2185",
+  copy_desc: "",
+  barcode_nmbr: "2185",
+  status_cd: "out",
+  status_begin_dt: "2006-06-09 10:17:18",
+  due_back_dt: "2006-06-30",
+  mbrid: "1035",
+  renewal_count: "0",
+  create_dt: "2005-05-24 20:08:39",
+  last_change_dt: "2023-01-27 10:56:54",
+  last_change_userid: "4",
+  material_cd: "2",
+  collection_cd: "6",
+  call_nmbr1: "Bücherei",
+  call_nmbr2: "",
+  call_nmbr3: "",
+  title: "Die Wilden Fußballkerle. Bd. 09: Joschka, die siebte Kavallerie",
+  title_remainder: "",
+  responsibility_stmt: "",
+  author: "Masannek, Joachim",
+  topic1: "Fußball",
+  topic2: "Teamgeist",
+  topic3: "",
+  topic4: "",
+  topic5: "",
+  opac_flg: "Y",
 };
 
 export default async function handler(
@@ -48,19 +88,50 @@ export default async function handler(
 ) {
   if (req.method === "POST") {
     //TEST RUN
-    addBook(prisma, sampleBook);
+    //addBook(prisma, sampleBook);
+    var importedBooksCount = 0;
     try {
       const booklist = req.body as any;
-      const books = booklist[2].data;
+      console.log("Booklist", booklist);
+      const books = booklist.biblio[2].data;
+      const bookStatus = booklist.biblio_hist[2].data;
+      const bookExtraFields = booklist.fields[2].data;
+      //console.log("Received data", books, bookStatus, bookExtraFields);
       const migratedBooks = books?.map((u: any) => {
-        const book = sampleBook;
-        //addBook(prisma, book);
+        //map the fields from OpenBiblio correctly:
+        /*console.log(
+          "Importing book nr " + importedBooksCount + ", which is ",
+          u
+        );*/
+        //console.log("Importing book", u);
+        const book = {
+          id: parseInt(u.bibid),
+          rentalStatus: (u.status_cd ??= "out"),
+          rentedDate: (u.status_begin_dt ??= new Date()),
+          dueDate: (u.due_back_dt ??= new Date()),
+          renewalCount: parseInt((u.renewal_count ??= 0)),
+          title: (u.title ??= "FEHLER Titel nicht importiert"),
+          subtitle: (u.title_remainder ??= "FEHLER Titel nicht importiert"),
+          author: (u.author ??= "FEHLER Autor nicht importiert"),
+          topics:
+            (u.topic1 ??= " ") +
+            ";" +
+            (u.topic2 ??= " ") +
+            ";" +
+            (u.topic3 ??= " ") +
+            ";" +
+            (u.topic4 ??= " ") +
+            ";" +
+            (u.topic5 ??= " "),
+          imageLink: "",
+        } as BookType;
+        console.log("Adding BookType book", book);
+        addBook(prisma, book);
+        importedBooksCount++;
         return book;
       });
-      console.log(migratedBooks);
-      res
-        .status(200)
-        .json({ data: "User " + JSON.stringify(migratedBooks) + " created" });
+
+      res.status(200).json({ data: importedBooksCount + " Books created" });
     } catch (error) {
       console.log(error);
       res.status(400).json({ data: "ERROR: " + error });
