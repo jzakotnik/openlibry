79c99be116a9590c78cd220f304a57cb8e6a4067
jzakotnik <jure@zakotnik.de>
2023-04-29 22:22:34 +0200

    introduced accordion

--- components/user/UserAdminList.tsx
+++ components/user/UserAdminList.tsx
@@ -1,26 +1,19 @@
-import { Typography, IconButton, ListItemButton } from "@mui/material";
-import { useState } from "react";
+import Typography from "@mui/material/Typography";
 
-import InputIcon from "@mui/icons-material/Input";
-
-import {
-  List,
-  ListItem,
-  ListItemText,
-  ListItemAvatar,
-  Avatar,
-} from "@mui/material";
+import { Avatar, Grid } from "@mui/material";
 
 import palette from "@/styles/palette";
 import { UserType } from "@/entities/UserType";
 
-export default function UserAdminList({
-  users,
-  rentals,
-  searchString,
-  selectItem,
-  handleEditUser,
-}: any) {
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+import UserDetailsCard from "./UserDetailsCard";
+
+export default function UserAdminList({ users, rentals, searchString }: any) {
   //attach amount of rented books to the user
   const rentalAmount: { [key: number]: number } = {};
 
@@ -31,51 +24,53 @@ export default function UserAdminList({
   });
 
   return (
-    <List dense={true}>
+    <div>
       {users.map((u: UserType) => {
         if (
           u.lastName.toLowerCase().includes(searchString.toLowerCase()) ||
           u.firstName.toLowerCase().includes(searchString.toLowerCase())
         )
           return (
-            <ListItem
-              key={u.id}
-              secondaryAction={
-                <IconButton
-                  onClick={() => handleEditUser(u.id)}
-                  edge="end"
-                  aria-label="delete"
+            <Accordion key={u.id}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Grid
+                  container
+                  direction="row"
+                  alignItems="center"
+                  justifyContent="flex-start  "
+                  sx={{ px: 10 }}
                 >
-                  <InputIcon />
-                </IconButton>
-              }
-            >
-              <ListItemButton onClick={() => selectItem(u.id!.toString())}>
-                <ListItemAvatar>
-                  <Avatar>
-                    {rentalAmount[u.id!] != undefined ? rentalAmount[u.id!] : 0}
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary={
-                    "Klasse " +
-                    u.schoolGrade +
-                    ", Lehrkaft " +
-                    u.schoolTeacherName
-                  }
-                  primaryTypographyProps={{
-                    variant: "caption",
-                    color: palette.secondary.light,
-                  }}
-                  secondaryTypographyProps={{
-                    color: palette.primary.main,
-                  }}
-                  secondary={u.lastName + ", " + u.firstName}
+                  <Grid item sx={{ px: 3 }}>
+                    {rentalAmount[u.id!] != undefined ? (
+                      <Avatar sx={{ bgcolor: palette.secondary.dark }}>
+                        {rentalAmount[u.id!]}
+                      </Avatar>
+                    ) : (
+                      <Avatar sx={{ bgcolor: palette.secondary.light }}>
+                        0
+                      </Avatar>
+                    )}
+                  </Grid>
+                  <Grid>
+                    <Typography>{u.lastName + ", " + u.firstName}</Typography>
+                  </Grid>
+                </Grid>
+              </AccordionSummary>
+              <AccordionDetails>
+                <UserDetailsCard
+                  user={u}
+                  rentals={rentals.filter(
+                    (r: any) => parseInt(r.userid) == u.id
+                  )}
                 />
-              </ListItemButton>
-            </ListItem>
+              </AccordionDetails>
+            </Accordion>
           );
       })}
-    </List>
+    </div>
   );
 }
