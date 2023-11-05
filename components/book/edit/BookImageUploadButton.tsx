fbea94b1815189e875113f4be705e2106c2b3655
jzakotnik <jure@zakotnik.de>
2023-11-05 22:19:03 +0100

    debugging

--- components/book/edit/BookImageUploadButton.tsx
+++ components/book/edit/BookImageUploadButton.tsx
@@ -38,7 +38,12 @@ const BookImageUploadButton = (props: any): any => {
         onInput={handleChange}
       >
         <PhotoCamera />
-        <input id="upload-image" hidden type="file" />
+        <input
+          id="upload-image"
+          hidden
+          accept="image/*;capture=camera"
+          type="file"
+        />
       </IconButton>
     </Stack>
   );
