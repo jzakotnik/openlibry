ddf391d218b4f1624fb997645508d55e9cbe0ceb
jzakotnik <jure@zakotnik.de>
2023-12-17 12:50:09 +0100

    trying excel

--- pages/api/excel/index.ts
+++ pages/api/excel/index.ts
@@ -2,7 +2,6 @@ import { BookType } from "@/entities/BookType";
 import { getAllBooks } from "@/entities/book";
 import { PrismaClient } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
-import xlsx from "node-xlsx";
 
 const prisma = new PrismaClient();
 
@@ -17,21 +16,17 @@ export default async function handle(
     ["foo", "bar", new Date("2014-02-19T14:30Z"), "0.3"],
     ["baz", null, "qux"],
   ];
-  var buffer = xlsx.build([{ name: "mySheetName", data: data }]);
 
   switch (req.method) {
     case "GET":
       try {
         const books = (await getAllBooks(prisma)) as Array<BookType>;
         books.map((b: BookType) => {});
-        const data = [HEADER_ROW, DATA_ROW_1];
 
-        const buffer = await writeXlsxFile(data as any, { buffer: true });
         if (!books)
           return res.status(400).json({ data: "ERROR: Books not found" });
         res.writeHead(200, {
           "Content-Type": "application/vnd.ms-excel",
-          "Content-Length": buffer.size,
         });
       } catch (error) {
         console.log(error);
