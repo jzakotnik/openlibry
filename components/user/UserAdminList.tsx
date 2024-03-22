22c4cce4e86ae18fe70420e7e06d781b0079c206
jzakotnik <jure@zakotnik.de>
2024-03-22 17:03:48 +0100

    update accordion

--- components/user/UserAdminList.tsx
+++ components/user/UserAdminList.tsx
@@ -1,6 +1,6 @@
 import Typography from "@mui/material/Typography";
 
-import { Avatar, Grid } from "@mui/material";
+import { Avatar, Checkbox, Grid, Paper } from "@mui/material";
 
 import { UserType } from "@/entities/UserType";
 import palette from "@/styles/palette";
@@ -44,47 +44,76 @@ export default function UserAdminList({
           u.id!.toString().includes(lowerCaseSearch)
         )
           return (
-            <Accordion key={u.id}>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-              >
-                <Grid
-                  container
-                  direction="row"
-                  alignItems="center"
-                  justifyContent="flex-start  "
-                  sx={{ px: 10 }}
-                >
-                  <Grid item sx={{ px: 3 }}>
-                    {rentalAmount[u.id!] != undefined ? (
-                      <Avatar sx={{ bgcolor: palette.secondary.dark }}>
-                        {rentalAmount[u.id!]}
-                      </Avatar>
-                    ) : (
-                      <Avatar sx={{ bgcolor: palette.secondary.light }}>
-                        0
-                      </Avatar>
-                    )}
-                  </Grid>
-                  <Grid>
-                    <Typography>{u.lastName + ", " + u.firstName}</Typography>
-                    <Typography variant="caption">
-                      {"Klasse " + u.schoolGrade + " - " + u.schoolTeacherName}
-                    </Typography>
-                  </Grid>
+            <Paper sx={{ mt: 0.5 }}>
+              <Grid container direction="row">
+                <Grid item xs={1} sx={{ width: "100%", height: "100%" }}>
+                  <Checkbox
+                    checked={true}
+                    onChange={() => {
+                      console.log("done");
+                    }}
+                    inputProps={{ "aria-label": "controlled" }}
+                  />
                 </Grid>
-              </AccordionSummary>
-              <AccordionDetails>
-                <UserDetailsCard
-                  user={u}
-                  rentals={rentals.filter(
-                    (r: any) => parseInt(r.userid) == u.id
-                  )}
-                />
-              </AccordionDetails>
-            </Accordion>
+                <Grid item xs={11} sx={{ width: "100%", height: "100%" }}>
+                  <Accordion
+                    elevation={0}
+                    sx={{
+                      width: "100%",
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
+                    key={u.id}
+                  >
+                    <AccordionSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls="panel1a-content"
+                      id="panel1a-header"
+                    >
+                      {" "}
+                      <Grid
+                        container
+                        direction="row"
+                        alignItems="center"
+                        justifyContent="flex-start  "
+                        sx={{ px: 10 }}
+                      >
+                        <Grid item sx={{ px: 3 }}>
+                          {rentalAmount[u.id!] != undefined ? (
+                            <Avatar sx={{ bgcolor: palette.secondary.dark }}>
+                              {rentalAmount[u.id!]}
+                            </Avatar>
+                          ) : (
+                            <Avatar sx={{ bgcolor: palette.secondary.light }}>
+                              0
+                            </Avatar>
+                          )}
+                        </Grid>
+                        <Grid>
+                          <Typography>
+                            {u.lastName + ", " + u.firstName}
+                          </Typography>
+                          <Typography variant="caption">
+                            {"Klasse " +
+                              u.schoolGrade +
+                              " - " +
+                              u.schoolTeacherName}
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <UserDetailsCard
+                        user={u}
+                        rentals={rentals.filter(
+                          (r: any) => parseInt(r.userid) == u.id
+                        )}
+                      />
+                    </AccordionDetails>
+                  </Accordion>
+                </Grid>
+              </Grid>
+            </Paper>
           );
       })}
     </div>
