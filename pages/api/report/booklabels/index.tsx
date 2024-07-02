6bd5b06a52d06de2fc5a418a256d362141522f2e
jzakotnik <jure@zakotnik.de>
2024-07-02 22:58:12 +0200

    updated jsbarcode for book labels

--- pages/api/report/booklabels/index.tsx
+++ pages/api/report/booklabels/index.tsx
@@ -10,6 +10,8 @@ import ReactPDF, {
   View,
 } from "@react-pdf/renderer";
 import type { NextApiRequest, NextApiResponse } from "next";
+import sharp from "sharp";
+import { Readable } from "stream";
 const { join } = require("path");
 
 const prisma = new PrismaClient();
@@ -20,6 +22,54 @@ var data = fs.readFileSync(
     encoding: "base64",
   }
 );
+
+function textToBase64Barcode() {
+  //this must be a joke
+  var JsBarcode = require("jsbarcode");
+  const { DOMImplementation, XMLSerializer } = require("xmldom");
+  const xmlSerializer = new XMLSerializer();
+  const document = new DOMImplementation().createDocument(
+    "http://www.w3.org/1999/xhtml",
+    "html",
+    null
+  );
+  const svgNode = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+
+  JsBarcode(svgNode, "1234", {
+    xmlDocument: document,
+  });
+  //console.log(xmlSerializer.serializeToString(svgNode));
+  const svgBuffer = xmlSerializer.serializeToString(svgNode);
+  console.log("rendering png from svg");
+  /*const resizedSVG = sharp(Buffer.from(svgBuffer))
+    .resize(200, 200)
+    .jpeg()
+    .toBuffer();
+
+  console.log(resizedSVG);
+  return resizedSVG;*/
+  const svgToPng = (svgString: string) => {
+    return new Promise((resolve, reject) => {
+      const svgStream = new Readable();
+      svgStream.push(svgString);
+      svgStream.push(null);
+      svgStream.pipe(
+        sharp()
+          .png()
+          .toBuffer((err, buffer) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(buffer);
+            }
+          })
+      );
+    });
+  };
+  const test = svgToPng(svgBuffer) as any;
+  console.log("Test", test.toString("base64"));
+  return test.toString("base64");
+}
 
 const styles = StyleSheet.create({
   page: {
@@ -52,8 +102,12 @@ const styles = StyleSheet.create({
     fontSize: 12,
   },
 });
+
 const Label = ({ b }: any) => {
   //console.log(b.id);
+  const barcode = textToBase64Barcode();
+
+  console.log("Rendering barcode on pdf", barcode);
   return (
     <View style={styles.section} wrap={false}>
       <Image
@@ -61,6 +115,10 @@ const Label = ({ b }: any) => {
         style={{ width: "1cm", height: "1cm" }}
       />
       <View style={styles.text} wrap={false}>
+        <Image
+          src={"data:image/png;base64, " + data}
+          style={{ width: "5cm", height: "1cm" }}
+        />
         <Text style={styles.booknr}>{b.id}</Text>
 
         <Text>{b.title}</Text>
