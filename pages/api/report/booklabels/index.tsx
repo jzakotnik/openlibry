f0909458b0ef7008390ce1960c696f11a1bafe8b
jzakotnik <jure@zakotnik.de>
2024-07-03 23:48:10 +0200

    removing old code

--- pages/api/report/booklabels/index.tsx
+++ pages/api/report/booklabels/index.tsx
@@ -11,7 +11,6 @@ import ReactPDF, {
 } from "@react-pdf/renderer";
 import bwipjs from "bwip-js";
 import type { NextApiRequest, NextApiResponse } from "next";
-import sharp from "sharp";
 const { join } = require("path");
 
 const prisma = new PrismaClient();
@@ -51,61 +50,10 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
   },
   booknr: {
+    padding: 2,
     fontSize: 12,
   },
 });
-
-async function textToBase64Barcode(id: string) {
-  //this must be a joke
-  var JsBarcode = require("jsbarcode");
-  const { DOMImplementation, XMLSerializer } = require("xmldom");
-  const xmlSerializer = new XMLSerializer();
-  const document = new DOMImplementation().createDocument(
-    "http://www.w3.org/1999/xhtml",
-    "html",
-    null
-  );
-  const svgNode = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-
-  JsBarcode(svgNode, "1234", {
-    xmlDocument: document,
-  });
-  //console.log(xmlSerializer.serializeToString(svgNode));
-  const svgBuffer = xmlSerializer.serializeToString(svgNode);
-  //console.log("rendering png from svg");
-  const resizedSVG = await sharp(Buffer.from(svgBuffer))
-    .resize(200, 200)
-    .png()
-    .toBuffer();
-
-  //console.log("sharped png", resizedSVG);
-  console.log(`data:image/png;base64,${resizedSVG.toString("base64")}`);
-  return resizedSVG;
-}
-
-const Label = async ({ b }: any) => {
-  //console.log(b.id);
-
-  //console.log("Rendering barcode on pdf", barcode);
-  return (
-    <View style={styles.section} wrap={false}>
-      <Image
-        src={"data:image/jpg;base64, " + data}
-        style={{ width: "1cm", height: "1cm" }}
-      />
-      <View style={styles.text} wrap={false}>
-        <Image
-          src={"data:image/png;base64, " + data}
-          style={{ width: "5cm", height: "1cm" }}
-        />
-        <Text style={styles.booknr}>{b.id}</Text>
-
-        <Text>{b.title}</Text>
-        <Text>Eigentum der Schulbücherei</Text>
-      </View>
-    </View>
-  );
-};
 
 const generateBarcode = async (books: Array<BookType>) => {
   const result = "";
@@ -122,13 +70,13 @@ const generateBarcode = async (books: Array<BookType>) => {
 
       return (
         <div key={b.id!}>
-          <Text>{b.title}</Text>
-          <Text>Eigentum der Schulbücherei</Text>
+          <Text style={styles.booknr}>{b.title}</Text>
           <Image
             key={b.id}
             src={"data:image/png;base64, " + (await png.toString("base64"))}
-            style={{ width: "3cm", height: "1cm" }}
+            style={{ width: "4cm", height: "1cm" }}
           />
+          <Text>Eigentum der Schulbücherei</Text>
         </div>
       );
     })
