91322475b2d609fa371d2a1afc84a77535833fb4
jzakotnik <jure@zakotnik.de>
2024-09-24 22:30:53 +0200

    fixed API response to return word

--- pages/api/report/reminder.tsx
+++ pages/api/report/reminder.tsx
@@ -3,10 +3,9 @@ import { getAllBooks } from "@/entities/book";
 import { PrismaClient } from "@prisma/client";
 
 import Docxtemplater from "docxtemplater";
+import fs from "fs";
+import { join } from "path";
 import PizZip from "pizzip";
-const { join } = require("path");
-
-//import template from "./mahnung-template.docx";
 
 import { NextApiRequest, NextApiResponse } from "next";
 
@@ -17,10 +16,11 @@ const testReplacement = {
   responsible_name: "Mickey",
   responsible_contract: "test@test.de",
 };
-var fs = require("fs");
-var template = fs.readFileSync(
+
+const template = fs.readFileSync(
   join(process.cwd(), "/public/mahnung-template.docx")
 );
+//console.log("Template", template);
 
 export default async function handle(
   req: NextApiRequest,
@@ -33,26 +33,26 @@ export default async function handle(
         const allbooks = (await getAllBooks(prisma)) as Array<BookType>;
 
         try {
-          let response = await fetch(template);
-          let data = await response.arrayBuffer();
-          let zip = new PizZip(data);
-          let templateDoc = new Docxtemplater(zip, {
+          //let data = await template.arrayBuffer();
+          const zip = new PizZip(template);
+          const templateDoc = new Docxtemplater(zip, {
             paragraphLoop: true,
             linebreaks: true,
           });
 
           templateDoc.render(testReplacement);
           const generatedDoc = templateDoc.getZip().generate({
-            type: "blob",
+            type: "nodebuffer",
             mimeType:
               "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
             compression: "DEFLATE",
           });
+          console.log("Generated doc", generatedDoc);
           res.writeHead(200, {
-            "Content-Type": "application/xml",
+            "Content-Type": "application/msword",
           });
 
-          res.status(200).json(generatedDoc);
+          res.status(200).send(generatedDoc);
         } catch (error) {
           console.log("Error: " + error);
         }
