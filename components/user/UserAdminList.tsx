ba5b0ad969226c6e2b8d76dce06670f45202d0e5
jzakotnik <jure@zakotnik.de>
2025-08-29 15:34:47 +0200

    fixed misaligned user list

--- components/user/UserAdminList.tsx
+++ components/user/UserAdminList.tsx
@@ -35,11 +35,20 @@ export default function UserAdminList({
 
   const rentalAmount: { [key: number]: number } = {};
 
-  const filterUserSub = (users: Array<UserType>, searchString: string, rentals: Array<RentalsUserType>, exactMatch: boolean = false) => {
-    let [filteredUsers, exactMatchRes] =
-      filterUsers(users, searchString, rentals, exactMatch);
+  const filterUserSub = (
+    users: Array<UserType>,
+    searchString: string,
+    rentals: Array<RentalsUserType>,
+    exactMatch: boolean = false
+  ) => {
+    let [filteredUsers, exactMatchRes] = filterUsers(
+      users,
+      searchString,
+      rentals,
+      exactMatch
+    );
     return filteredUsers;
-  }
+  };
 
   //initialise user checked array
 
@@ -76,7 +85,7 @@ export default function UserAdminList({
               alignItems="center"
               justifyContent="flex-start"
             >
-              <Grid size={{ xs: 1 }} sx={{ width: "100%", height: "100%" }}>
+              <Grid size={{ xs: 1 }}>
                 <Checkbox
                   checked={checkBoxValue ? checkBoxValue : false}
                   id={userID}
@@ -84,10 +93,9 @@ export default function UserAdminList({
                   onChange={() => {
                     setChecked({ ...checked, [userID]: !checkBoxValue });
                   }}
-                  inputProps={{ "aria-label": "controlled" }}
                 />
               </Grid>
-              <Grid size={{ xs: 11 }} sx={{ width: "100%", height: "100%" }}>
+              <Grid size={{ xs: 11 }}>
                 <Accordion
                   elevation={0}
                   sx={{
