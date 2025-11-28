7f1b97fc650d8101aacbd2d78b2388b9af3f143c
jzakotnik <jure@zakotnik.de>
2025-11-28 15:17:27 +0100

    sample tests for report of rentals

--- cypress/e2e/report.cy.ts
+++ cypress/e2e/report.cy.ts
@@ -9,5 +9,10 @@ describe("Navigation", () => {
     cy.visit("http://localhost:3000/");
     cy.get("[data-cy=index_reports_button]").click();
     cy.get("[data-cy=report-card-rentals-button]").should("be.visible");
+    cy.get("[data-cy=report-card-rentals-button]").click();
+    cy.get("[data-cy=rentals-datagrid]").should("be.visible");
+    cy.get('[data-cy="rentals-error"]').should("not.exist");
+    cy.get('[data-cy="rentals-no-data"]').should("not.exist"); //no errors
+    cy.get(".MuiDataGrid-row").should("have.length.gt", 0); //has some rows
   });
 });
