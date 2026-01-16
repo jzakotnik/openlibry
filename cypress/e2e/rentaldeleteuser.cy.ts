8ebe2173a3385c8fe3cd4bab35fa1587f1771b7c
jzakotnik <jure@zakotnik.de>
2026-01-16 14:23:18 +0100

    updated user rental test

--- cypress/e2e/rentaldeleteuser.cy.ts
+++ cypress/e2e/rentaldeleteuser.cy.ts
@@ -9,7 +9,7 @@ describe("Rental cleanup on user deletion", () => {
     cy.cleanupDatabase();
   });
 
-  it("should rent a book, delete the user, and verify the book is available again", () => {
+  it("should rent a book, delete the user, and verify the book is also deleted", () => {
     cy.log(Cypress.env("user"));
 
     // Navigate to the rental screen
@@ -40,6 +40,14 @@ describe("Rental cleanup on user deletion", () => {
 
     // Search for an available book (BookRentalList component)
     cy.get("[data-cy=book_search_input]").should("be.visible").type("Dorf");
+
+    // Store the book title for later verification
+    cy.get("[data-cy^=book_item_]")
+      .first()
+      .invoke("text")
+      .then((bookText) => {
+        cy.wrap(bookText).as("rentedBookText");
+      });
 
     // Find an available book (not rented) and click the rent button
     cy.get("[data-cy^=book_item_]")
@@ -86,21 +94,32 @@ describe("Rental cleanup on user deletion", () => {
     cy.url().should("include", "/user");
     cy.url().should("not.include", "/user/");
 
-    // Now navigate back to rental screen to verify the book is available again
+    // Now navigate to book list to verify the book is also deleted
     cy.visit("http://localhost:3000/");
-    cy.get("[data-cy=index_rental_button]").click();
-    cy.url().should("include", "/rental");
+    cy.get("[data-cy=index_book_button]").click();
 
-    // Search for the book that was previously rented
-    cy.get("[data-cy=book_search_input]").should("be.visible").type("Dorf");
+    // Verify book list page is loaded
+    cy.url().should("include", "/book");
+    cy.get("[data-cy=rental_input_searchbook]").should("be.visible");
 
-    // Verify the book is now available (has the rent button visible, not showing as rented)
-    cy.get("[data-cy^=book_item_]")
-      .first()
-      .within(() => {
-        // The rent button should be visible, indicating the book is available
-        cy.get("[data-cy^=book_rent_button_]").should("be.visible");
-      });
+    // Search for the book that was rented by the deleted user
+    cy.get("[data-cy=rental_input_searchbook]").type("Dorf");
+
+    // Wait for search results to update
+    cy.wait(1000);
+
+    // Verify the book is no longer in the list (it should be deleted along with the user)
+    cy.get("body").then(($body) => {
+      if ($body.find("[data-cy^=book_item_]").length > 0) {
+        // If there are books, verify the rented book is not among them
+        cy.get("[data-cy^=book_item_]").should("not.exist");
+      } else {
+        // No books found matching the search - this is expected
+        cy.log(
+          "No books found matching 'Dorf' - book was successfully deleted with user"
+        );
+      }
+    });
 
     // Navigate to user list to verify it renders correctly
     cy.visit("http://localhost:3000/");
