4630d17cbcbd3fbfddbb004f6bad7c800e23658e
jzakotnik <jure@zakotnik.de>
2026-01-25 16:09:59 +0100

    fixed backup button to admin button prop

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -5,18 +5,18 @@ import TopBar from "./TopBar";
 
 interface LayoutProps {
   publicView?: boolean;
-  showBackupButton?: boolean;
+  showAdminButton?: boolean;
   children: React.ReactNode;
 }
 
 export default function Layout({
   publicView = false,
-  showBackupButton = true,
+  showAdminButton = true,
   children,
 }: LayoutProps) {
   return (
     <div>
-      {!publicView && <TopBar showBackupButton={showBackupButton} />}
+      {!publicView && <TopBar showAdminButton={showAdminButton} />}
       <Container maxWidth="lg">{children}</Container>
       <Footer />
     </div>
