b144f0f2531c1a16e1c8c660451a658a486ae07e
Johannes <johannes@wimmweg.at>
2026-04-10 10:50:55 +0200

    Barcode am Ausweis um Steuerzeichen erweiterbar (closes Issue #369)

--- pages/api/report/userlabels/index.tsx
+++ pages/api/report/userlabels/index.tsx
@@ -94,6 +94,18 @@ const barcodeMinLength: number =
     ? parseInt(process.env.BARCODE_MINCODELENGTH)
     : DEFAULT_BARCODE_MINCODELENGTH;
 
+// Postfix & Prefix for Barcode
+// Hilfs-Funktion zum "Auflösen" von Hex-Strings aus Barcode Prefix/Suffix
+const parseEnv = (str: string) => 
+  str.replace(/\\x([0-9A-Fa-f]{2})/g, (_, hex) => String.fromCharCode(parseInt(hex, 16)))
+     .replace(/\\r/g, "\r")
+     .replace(/\\n/g, "\n")
+     .replace(/\\t/g, "\t");
+
+const barcodePrefix = parseEnv(process.env.USERLABEL_BARCODE_PREFIX || "");
+const barcodeSuffix = parseEnv(process.env.USERLABEL_BARCODE_SUFFIX || "");
+
+
 // =============================================================================
 // Styles
 // =============================================================================
